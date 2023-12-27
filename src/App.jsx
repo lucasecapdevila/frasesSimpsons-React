f32e3878ddf5d5a8d71f4351291ee112e274ba08
Lucas Capdevila <lcapdevila60@gmail.com>
2023-12-27 15:50:18 -0300

    se completa maquetado de frase de Ania

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Frase from './components/Frase'
+import { Button } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <main className='d-flex flex-column justify-content-center align-items-center my-4'>
+        <div>
+          <img className='img-fluid' src="../public/logosimpson.png" alt="Logo de Los Simpson" />
+        </div>
+
+        <Frase/>
+
+        <Button variant='success' className='mt-2'>Obtener frase</Button>
+      </main>
     </>
   )
 }
---
+++ src/components/Frase.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Frase = () => {
+  return (
+    <>
+      <h1 className='mt-5'>Ania</h1>
+      <img src="https://pbs.twimg.com/media/Eu06gv9XMAkGDsd.jpg" alt="Lisa leyendo la carta de Ania" />
+      <blockquote className='w-50 my-2 lead text-center'>Querida Lisa: Al escribir esto estoy triste, nuestro presidente ha sido derrocado Y REEMPLAZADO POR EL BENÉVOLO GENERAL KROLL, TODOS AMAMOS A KROLL Y A SU GLORIOSO RÉGIMEN. CON AMOR, NIÑITA</blockquote>
+    </>
+  );
+};
+
+export default Frase;
